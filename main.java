3727455ef96de58d68c82aa3dcc2deabba97aed3
diego <diego@email.com>
2025-06-27 04:32:52 -0600

    version 0.1

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class main{
+	public static void main(String []args){
+		Scanner leer = new Scanner();
+		int a;
+		System.out.println("escribe un numero: ");
+		a=leer.nextInt();
+		System.out.println(a);
+	}
+}
